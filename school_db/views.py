1f62075957aaa2fcccd123421f068088324a4212
TinTooth <boconne4@gmail.com>
2022-10-04 11:17:11 -0500

    problem 1 and 2 completed

--- school_db/views.py
+++ school_db/views.py
@@ -59,7 +59,11 @@ SELECT `school_db_student`.`id`,
 # Print out each student's full name and gpa to the terminal
 def problem_one(request):
 
-    return complete(request)
+  students = Student.objects.filter(gpa__gt = 3).order_by('-gpa')
+  for student in students:
+    print(f'Full Name: {student.first_name} {student.last_name} GPA: {student.gpa}')
+    
+  return complete(request)
 
 
 # Supporting Query Method Documentation:
@@ -97,8 +101,13 @@ SELECT `school_db_student`.`id`,
 # Order by hire date ascending
 # Print out the instructor's full name and hire date to the terminal
 def problem_two(request):
-
-    return complete(request)
+  instructors = Instructor.objects.filter(hire_date__lt = '2010-01-01').order_by('hire_date')
+  print()
+  for instructor in instructors:
+    print(f'Full Name: {instructor.first_name} {instructor.last_name}')
+    print(f'Hire Date: {instructor.hire_date}')  
+  print()
+  return complete(request)
 
 
 # Supporting Query Method Documentation:
